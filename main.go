46e4fe289cce1de129c1864a17316094f075bc83
lilin <lilin@myrual.me>
2019-02-27 19:41:34 +0800

    remove config file and restore to first version

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"log"
-	"./config"
 	"github.com/MooooonStar/mixin-sdk-go/messenger"
 )
 
@@ -32,9 +31,32 @@ func (l *Listener) OnMessage(ctx context.Context, msg messenger.MessageView, use
 	}
 }
 
+const (
+	UserId    = "595053f7-c441-4266-a8e7-ca6eb6123d63"
+	PinCode   = "518443"
+	SessionId = "0b5ab266-0da3-43e1-b24a-b4d11671c134"
+	PinToken  = "O2D8XVoPIkCPDCVSy7wmRLxV/AqELZl//Is2ZY0SLHtYtAHsEDlL8ie7EcvjoPeJMIPsOWdIXhMDuJdL2Q2kUAxutGA4//dw1w/EEqCUonWVGAGOEw2GPK2q8yYkkiWuS+ygD6ze+U7dd8SYrmAFeWoqIO2KnXaUBY5w9sA1b8A="
+	//please delele the blank of PrivateKey the before each line
+	PrivateKey = `-----BEGIN RSA PRIVATE KEY-----
+MIICXAIBAAKBgQCpA3wZQI0txHzbXGh7PmWKUAtIj3ddS1XynqFforCC+0DQkI17
+dI5eROKruJL88+fI3MMFz/z4v7n8slWVcA7wDtGmaRM/d0fwNmVpwfBD7s7KXujg
+DG/JYNL3Jasl/oyX7d/8/2qIeK5wRo26NM9TE11yJWnUJkeaHWqvwd1CKQIDAQAB
+AoGAcDNLCCyNTKvbQ3j2N5L1r7txsirj0AB6P0XF1YpYaavInGzKMAHxnW1XmS5A
+DkMRRYwgv9QSmUQWgUYWgM1SbD4uwwmjoZW6SJXT6nFzDfNVgiOIOw65qwCwodpI
+5yedcJWU2/SgMg6koesl/oG2h3kUVufa1cZLLYc7B+ozP7ECQQDwNlz+sKopwTy8
+b6yfdKGMU2ilxCMZnxTdXGNT6e1EqChup9qqupjLvHI5ww7tUUfyMibbxFyXFps+
+aAGuzRR9AkEAtB8vB2MKdZF9nlUhYtKg586sprfa4xm0e2RwfFqut5gSBBX0vmZ5
+mByJfi9UoC1uGMMHGfmNmzmETv/cUnywHQJASt/nJMxUfidpkyMMH7fvExp8qA73
+EelwS3+cxm9IMfpof/V6R3VaY0ceI7sVUAvh7TCMxDv00HOPeGha/e2W0QJBAIlu
+UZ+9Pbm+rK4mHjccppC37Ju4JFaqwj5zwC/hsPV8HiLn7bloztX00CNrUZJ1l09l
+XwG+mwEYvY7ZQe3eHTECQDo6eldTLlUW7Q6ALjwUzNTBaQ0Q/19zhXkd3SfYPXzt
+g07CDzMD0ynLxqpChz5oCThooLMIOhU+EMzceJfMmj0=
+-----END RSA PRIVATE KEY-----`
+)
+
 func main() {
 	ctx := context.Background()
-	m := messenger.NewMessenger(config.UserId, config.SessionId, config.PrivateKey)
+	m := messenger.NewMessenger(UserId, SessionId, PrivateKey)
 	//replace with your own listener
 	//go m.Run(ctx, messenger.DefaultBlazeListener{})
 	l := &Listener{m}
